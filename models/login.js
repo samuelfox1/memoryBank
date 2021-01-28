92b7968e983694d144c5212946f223b7bfcdd8b1
Samuel Fox <samueljasonfox@gmail.com>
2021-01-28 14:12:03 -0800

    fresh start

--- models/login.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (sequelize, DataTypes) {
-  var login = sequelize.define("login", {
-    user_name: DataTypes.STRING,
-    password: DataTypes.STRING,
-
-  });
-  return login;
-};
--- models/user.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (sequelize, DataTypes) {
-    var user = sequelize.define("user", {
-        first_name: DataTypes.STRING,
-        last_name: DataTypes.STRING,
-
-    });
-    return user;
-};
