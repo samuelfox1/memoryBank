24c6f54ff4ee9e7625cf60b0acdd09adeb768542
Mitchell Bean <mitchtroybean@gmail.com>
2021-01-29 00:15:43 -0800

    working on passing table identifiers

--- models/user-data.js
+++ models/user-data.js
@@ -1,5 +1,5 @@
 module.exports = function (sequelize, DataTypes) {
-  var User_Data = sequelize.define("user_data", {
+  var user_data = sequelize.define("user_data", {
     first_name: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -37,6 +37,15 @@ module.exports = function (sequelize, DataTypes) {
     //   },
     // },
   });
+  user_data.associate = function (models) {
+    // We're saying that a Post should belong to an Author
+    // A Post can't be created without an Author due to the foreign key constraint
+    user_data.belongsTo(models.all_user, {
+      foreignKey: {
+        allowNull: false,
+      },
+    });
+  };
   // return Test;
-  return User_Data;
+  return user_data;
 };
