3ab535b2dd656e76df279c97da10b5235061d46e
Andrew Anthony <a.anthony1835@gmail.com>
2021-01-29 10:05:57 -0800

    created basic front end for hom pages

---
+++ homePagesFrontEnd/userHome.js
@@ -0,0 +1,24 @@
+$(".dropdown-trigger").dropdown();
+
+
+$('#textarea1').val('New Text');
+M.textareaAutoResize($('#textarea1'));
+
+
+$("#memory").on("click", function () {
+    alert("Button works")
+})
+
+
+$("#submit").on("click", function () {
+    alert("Button works")
+})
+
+
+$("#edit").on("click", function () {
+    alert("Button works")
+})
+
+$("#delete").on("click", function () {
+    alert("Button works")
+})
