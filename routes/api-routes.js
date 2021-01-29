eec8b320f5cd54a529b0b38bdda53c1f6e3f1289
Samuel Fox <samueljasonfox@gmail.com>
2021-01-29 04:56:35 -0800

    added edge cases to start collecting and storing horoscope data from teh api. First checks to see if the table is empty (new user), then ckecks to see if the last entry has todays date. as long as the last entry in the table does not match todays date, make th eapi request and store the daat in the database.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -16,30 +16,79 @@ const options = {
     }
 };
 
+const horoscope = []
+
 // Routes
 // =============================================================
 module.exports = function (app) {
-    app.get("/api/aztro", function (req, res) {
+    app.get("/api/aztro/:id", async function (req, res) {
 
+        // when the get request is made...
+        // get todays date,
+        var today = await getToday()
+        //get the date of last entry in database
+        var lastEntry = await getLastEntryDate()
+        //if today does not equal last entry... 
+        if (today != lastEntry) {
+            //make the api request
+            var aztro = await makeApiRequest()
+            //clear contents of array
+            horoscope.length = 0
+            // push api results into array
+            horoscope.push(aztro)
+            //send the results back to client
+            res.json(horoscope)
+        } else {
+            //send the stored results back to the client
+            console.log('todays data already exists')
+            res.send(horoscope)
+        }
+    });
+};
 
+function getToday() {
+    return new Promise((resolve, reject) => {
+        let x = new Date();
+        let dd = String(x.getDate()).padStart(2, '0');
+        let mm = String(x.getMonth() + 1).padStart(2, '0'); //January is 0!
+        let yyyy = x.getFullYear();
+
+        x = mm + '/' + dd + '/' + yyyy;
+        resolve(x);
+    });
+}
+function getLastEntryDate() {
+    return new Promise((resolve, reject) => {
+        db.daily_history.findAll({
+            limit: 1,
+            order: [['createdAt', 'DESC']]
+
+        }).then((data) => {
+            if (data[0] === undefined) {
+                resolve(0)
+            } else {
+                let x = data[0].createdAt
+                let dd = String(x.getDate()).padStart(2, '0');
+                let mm = String(x.getMonth() + 1).padStart(2, '0'); //January is 0!
+                let yyyy = x.getFullYear();
+
+                x = mm + '/' + dd + '/' + yyyy;
+                resolve(x);
+            }
+        })
+    });
+}
+
+function makeApiRequest() {
+    return new Promise((resolve, reject) => {
         request(options, function (error, response, body) {
             if (error) throw new Error(error);
             let aztro = JSON.parse(body)
-            let date = new Date(aztro.current_date)
-            let year = date.getFullYear()
-            let month = date.getMonth()
-            let day = date.getDay()
-            console.log(date)
-            console.log(year)
-            console.log(month)
-            console.log(day)
 
-            db.daily_history.findOne()
             db.daily_history.create(aztro).then((data) => {
-
-                res.json(aztro)
+                console.log('line 85')
+                resolve(aztro)
             })
-
-        });
-    });
-};
+        })
+    })
+}
