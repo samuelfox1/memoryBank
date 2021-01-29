a56b5153810c93d84e461518416c91ade820b7a0
Andrew Anthony <a.anthony1835@gmail.com>
2021-01-29 14:39:08 -0800

    created routes

---
+++ public/js/login.js
@@ -0,0 +1,16 @@
+// const loginAccountBtn = $("#loginToAccountBtn");
+
+// $(".dropdown-trigger").dropdown();
+
+// loginAccountBtn.on("click", function (event) {
+//   event.preventDefault();
+//   console.log("logging into account");
+// });
+
+// function userLogin() {
+//   $.get("/login", function () {
+//     console.log("to login");
+//   });
+// }
+
+// userLogin();
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,8 +7,24 @@ module.exports = function (app) {
   app.get("/", function (req, res) {
     res.render("index");
   });
-  app.get("/test", function (req, res) {
-    res.render("test");
+  app.get("/login", function (req, res) {
+    res.render("login");
+  });
+
+  app.get("/history", function (req, res) {
+    res.render("history");
+  });
+
+  app.get("/create", function (req, res) {
+    res.render("create");
+  });
+
+  app.get("/home", function (req, res) {
+    res.render("userHome");
+  });
+
+  app.get("/review", function (req, res) {
+    res.render("userHome2");
   });
 
   app.post("/api/", function (req, res) {
