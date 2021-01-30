5856b4881ac9368cc0deaaf5313306702365cecb
Kevin Hsieh <khsieh95@gmail.com>
2021-01-30 15:39:15 -0800

    layouts of pages, and design

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,29 +1,29 @@
-// Requiring our models
-const db = require("../models");
+// // Requiring our models
+// const db = require("../models");
 
-// Routes
-// =============================================================
-module.exports = function (app) {
-  app.get("/", function (req, res) {
-    res.render("index");
-  });
-  app.get("/login", function (req, res) {
-    res.render("login");
-  });
+// // Routes
+// // =============================================================
+// module.exports = function (app) {
+//   app.get("/", function (req, res) {
+//     res.render("index");
+//   });
+//   app.get("/login", function (req, res) {
+//     res.render("login");
+//   });
 
-  app.get("/history", function (req, res) {
-    res.render("history");
-  });
+//   app.get("/history", function (req, res) {
+//     res.render("history");
+//   });
 
-  app.get("/create", function (req, res) {
-    res.render("create");
-  });
+//   app.get("/create", function (req, res) {
+//     res.render("create");
+//   });
 
-  app.get("/home", function (req, res) {
-    res.render("userHome");
-  });
+//   app.get("/home", function (req, res) {
+//     res.render("userHome");
+//   });
 
-  app.get("/review", function (req, res) {
-    res.render("userHome2");
-  });
-};
+//   app.get("/review", function (req, res) {
+//     res.render("userHome2");
+//   });
+// };
