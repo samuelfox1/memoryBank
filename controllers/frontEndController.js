fcdaaee103be3bad7426b1e703282dd173f4ba7d
Kevin Hsieh <khsieh95@gmail.com>
2021-01-30 22:30:02 -0800

    working on injecting handlebars

--- controllers/frontEndController.js
+++ controllers/frontEndController.js
@@ -6,7 +6,9 @@ router.get("/", function (req, res) {
   res.render("index");
 });
 router.get("/login", function (req, res) {
-  res.render("login");
+  res.render("login", {
+    user: req.session.user,
+  });
 });
 
 router.get("/history", function (req, res) {
@@ -20,7 +22,27 @@ router.get("/create", (req, res) => {
 });
 
 router.get("/home", function (req, res) {
-  res.render("userHome");
+  db.user_data
+    .findOne({
+      where: {
+        id: req.session.user.id,
+      },
+      include: [db.daily_history],
+    })
+    .then((data) => {
+      console.log(data, "00000000000000000000");
+      const jsonData = data.map((obj) => {
+        const jsonObj = obj.toJSON();
+
+        return jsonObj;
+      });
+
+      console.log(data.first_name);
+      var object = {
+        horoscope: jsonData,
+      };
+      res.render("userHome", object);
+    });
 });
 
 router.get("/review", function (req, res) {
