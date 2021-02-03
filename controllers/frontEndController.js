6244479e98a7e532d4e34986f0b36c4e5cec903a
Kevin Hsieh <khsieh95@gmail.com>
2021-02-03 12:24:15 -0800

    finishing touches

--- controllers/frontEndController.js
+++ controllers/frontEndController.js
@@ -4,37 +4,26 @@ const router = express.Router();
 const db = require("../models");
 const { Op } = require("sequelize");
 
-
-
 // / route to the welcome page
 router.get("/", function (req, res) {
   res.render("index");
 });
-
-
 
 // /login route to go to login page
 router.get("/login", function (req, res) {
   res.render("login", { user: req.session.user });
 });
 
-
-
 // /create route from welcome page to create user page
 router.get("/create", (req, res) => {
   res.render("create", { user: req.session.user });
 });
-
-
 
 //route to destroy active session cookie
 router.get("/logout", (req, res) => {
   req.session.destroy();
   res.send("logged out");
 });
-
-
-
 
 // /home route for after login user goes to their home page
 // users home page will always display the most recent data for that day
@@ -43,10 +32,10 @@ router.get("/home", async function (req, res) {
   db.daily_history
     .findOne({
       where: { createdAt: lastEntry.createdAt },
-      include: [db.user_data]
+      include: [db.user_data],
     })
     .then((data) => {
-      data.dataValues.createdAt = convertDate(data.dataValues.createdAt)
+      data.dataValues.createdAt = convertDate(data.dataValues.createdAt);
       const hbsObj = {
         histories: data.dataValues,
         users: data.dataValues.user_datum.dataValues,
@@ -54,8 +43,6 @@ router.get("/home", async function (req, res) {
       res.render("userHome", hbsObj);
     });
 });
-
-
 
 // /history route that the user can visit once logged in from the burger bar in the top right to view past entries order by most recent day
 router.get("/history", function (req, res) {
@@ -66,10 +53,8 @@ router.get("/history", function (req, res) {
       order: [["createdAt", "DESC"]],
     })
     .then((data) => {
+      // data[0].createdAt = convertDate(data[0].createdAt);
 
-      console.log(data[0].dataValues.createdAt, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-      data[0].dataValues.createdAt = convertDate(data[0].dataValues.createdAt)
-      console.log(data[0].dataValues.createdAt, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
       const jsonData = data.map((obj) => {
         const jsonObj = obj.toJSON();
         return jsonObj;
@@ -77,13 +62,10 @@ router.get("/history", function (req, res) {
       const hbsObj = {
         histories: jsonData,
       };
-      console.log(jsonData)
+      console.log(jsonData);
       res.render("history", hbsObj);
     });
 });
-
-
-
 
 router.get("/:term", (req, res) => {
   db.user_data
@@ -107,22 +89,22 @@ router.get("/:term", (req, res) => {
         data: jsonData,
       };
       res.render("testsearch", hbsObj);
-
     });
 });
-
 
 //returns the most recent entry for the logged in user
 function getLastEntry(data) {
   return new Promise((resolve, reject) => {
     db.daily_history
-      .findAll({ where: { userDatumId: data } },
+      .findAll(
+        { where: { userDatumId: data } },
         { limit: 1, order: [["createdAt", "DESC"]] }
       )
-      .then((data) => { resolve(data[0]) });
+      .then((data) => {
+        resolve(data[0]);
+      });
   });
 }
-
 
 // get todays date formatted as mm/dd/yyyy
 function convertDate(x) {
@@ -132,7 +114,5 @@ function convertDate(x) {
   x = mm + "/" + dd + "/" + yyyy;
   return x;
 }
-
-
 
 module.exports = router;
